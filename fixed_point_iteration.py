d632cf20e047dc40cd7ba7e496d4772de4d923ed
aswin16 <you@example.com>
2020-09-17 16:46:12 +0530

    added iter fn

--- fixed_point_iteration.py
+++ fixed_point_iteration.py
@@ -4,11 +4,9 @@ september 16 2020'''
 
 
 import numpy as np #numerical python library
+import sys
 
 
-def f(x):
-    
-    return np.exp(-2*x)
 
 def is_approx(x,y):
     '''Function to check
@@ -18,9 +16,13 @@ def is_approx(x,y):
     return (abs(x-y) < 0.0001)
 
 
-def fpa(fn,init):
+def fpi_recur(fn,init):
     '''Recursive implementation
-    of FIxed Point Iteration
+    of Fixed Point Iteration.
+    Returns error if it doesnt 
+    converge within the maximum
+    recursive depth of python
+    
     ARGS: function
     '''
     y = fn(init)
@@ -33,13 +35,36 @@ def fpa(fn,init):
 
     else:
         
-        fpa(fn,y)
+        fpi_recur(fn,y)
 
     
     
 
-fpa(f,0.731689)
+def fpi_iter(fn,num_iter, init_):
+    '''Iterative implementation
+    ARGS: function
+          number of iteration
+          init_-initial value
+    Returns: y-fixed point
+
+    '''
+    x = init_
+    for i in range(num_iter):
+        y =  fn(x)
+
+        if is_approx(x,y):
+            break
+
+        else:
+            x = y
+
+    return y
+
+
+print('iterative:',fpi_iter(lambda x: np.exp(-2*x),100,0.7831))
+print('recursive:')
+fpi_recur(lambda x: np.exp(-2*x),0.5)
 
 
 
-
+   
